96313c3abf2f03c9b936aaa812738ca8822f6b86
Joe Rouleau <rouleau.joseph@gmail.com>
2024-09-26 10:33:29 -0400

    Initial Commit

---
+++ MobileShowcase/MobileShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileShowcaseApp.swift
+//  MobileShowcase
+//
+//  Created by Joseph Rouleau on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MobileShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MobileShowcaseTests/MobileShowcaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  MobileShowcaseTests.swift
+//  MobileShowcaseTests
+//
+//  Created by Joseph Rouleau on 9/26/24.
+//
+
+import Testing
+@testable import MobileShowcase
+
+struct MobileShowcaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
